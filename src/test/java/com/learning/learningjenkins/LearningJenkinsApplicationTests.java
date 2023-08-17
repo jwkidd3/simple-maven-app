f9b87c45a0605bc33b5efea4879f4e1f967d64f9
John Kidd <jwkidd3@Johns-MacBook-Air.local>
2023-08-17 07:34:19 -0500

    initial

---
+++ src/test/java/com/learning/learningjenkins/LearningJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.learningjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
